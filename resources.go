10034ae7509fdab9d5d38f8782813c8829a2eddc
Dim <skyuphigh01@gmail.com>
2018-05-03 18:06:51 -0400

    Initial Commmit

---
+++ resources.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"io/ioutil"
+
+	jsoniter "github.com/json-iterator/go"
+)
+
+func cats() ([]string, error) {
+	data, err := ioutil.ReadFile("./assets/cats.json")
+	if err != nil {
+		return nil, err
+	}
+
+	var cats []string
+	err = jsoniter.Unmarshal(data, &cats)
+	if err != nil {
+		return nil, err
+	}
+
+	return cats, nil
+}
---
+++ server.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+
+	"github.com/kataras/iris"
+)
+
+func handle(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	cats, err := cats()
+	handle(err)
+	app := iris.New()
+
+	app.Get("/random-cat", func(ctx iris.Context) {
+		cat := cats[rand.Intn(len(cats)-1)]
+		ctx.JSON(iris.Map{"file": cat})
+	})
+
+	app.Run(iris.Addr(":8080"))
+}
