f7c17b8e1fd558d73328f3468901f0985a7cfbf3
Dim <skyuphigh01@gmail.com>
2018-05-03 18:43:33 -0400

    Add CORS

--- server.go
+++ server.go
@@ -17,6 +17,10 @@ func main() {
 	handle(err)
 	app := iris.New()
 
+	app.Use(func(ctx iris.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+	})
+
 	app.Get("/random-cat", func(ctx iris.Context) {
 		cat := cats[rand.Intn(len(cats)-1)]
 		ctx.JSON(iris.Map{"file": cat})
